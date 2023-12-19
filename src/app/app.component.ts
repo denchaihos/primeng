862341eb226194f94d75d8a2d6c1c72a108ace1a
Denchai <denchaihos@gmail.com>
2023-12-19 10:34:55 +0700

    dk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { ProgressBarModule } from 'primeng/progressbar';
+
 // For dynamic progressbar demo
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,ButtonModule,RadioButtonModule,ProgressBarModule],
+  imports: [CommonModule, RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
